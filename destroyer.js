08fb5192545f05dc1ad8928f81b43414538e95e6
spacescript <simonbengoehring@gmail.com>
2021-07-04 15:09:02 -0700

    Update destroyer.js

--- destroyer.js
+++ destroyer.js
@@ -1,7 +1,22 @@
 function destroyer(arr) {
-    return arr;
-  }
+    var modArgArr = [];
+    for(let i = 1; i < arguments.length; i++) {
+       modArgArr.push(arguments[i]);
+    }
+
+    var myArr = arguments[0];
+
+    for(let i = 0; i < myArr.length; i++) {
+        for(let j = 0; j < modArgArr.length; j++) {
+            if(myArr[i] === modArgArr[j]) {
+                delete myArr[i];
+            }
+        }
+    }
+
+    return myArr.filter(item => item !== null);
+}
   
 var sample = destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 
-console.log(sample(arguments));
+console.log(sample);
