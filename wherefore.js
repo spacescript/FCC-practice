2d5868ed7d2e588c04585fc44d87ab4b4fc764f6
spacescript <simonbengoehring@gmail.com>
2021-07-04 23:19:21 -0700

    Create wherefore.js

---
+++ wherefore.js
@@ -0,0 +1,12 @@
+function whatIsInAName(collection, source) {
+    var arr = [];
+    // Only change code below this line
+  
+  
+    // Only change code above this line
+    return arr;
+}
+  
+var result = whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+console.log(result);
