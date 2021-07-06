3ddc5a6eeee6f4980acdec6374781da268ac83ec
spacescript <simonbengoehring@gmail.com>
2021-07-05 18:40:56 -0700

    Update wherefore.js

--- wherefore.js
+++ wherefore.js
@@ -1,10 +1,20 @@
 function whatIsInAName(collection, source) {
     var arr = [];
     // Only change code below this line
-  
-  
+    var srcKeys = Object.keys(source);
+    return collection.filter(function(obj) {
+        for (let i = 0; i < srcKeys.length; i++) {
+            if (
+                !obj.hasOwnProperty(srcKeys[i]) ||
+                obj[srcKeys[i]] !== source[srcKeys[i]]
+            ) {
+                return false;
+            }
+        }
+        return true;
+    });
+
     // Only change code above this line
-    return arr;
 }
   
 var result = whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
