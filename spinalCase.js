39764c4def32a6f73028c898a804e43fe0031796
spacescript <simonbengoehring@gmail.com>
2021-07-06 17:09:11 -0700

    Update spinalCase.js

--- spinalCase.js
+++ spinalCase.js
@@ -1,7 +1,29 @@
 function spinalCase(str) {
-    return str;
+    // let reg = /[\W_]+/gm;
+
+
+    let regex = /\s+|_+/g;
+
+    str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+    // return str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+    return str.replace(regex,"-").toLowerCase();
+
+
+//       // Create a variable for the white space and underscores.
+//   var regex = /\s+|_+/g;
+
+//   // Replace low-upper case to low-space-uppercase
+//   str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+//   // Replace space and underscore with -
+//   return str.replace(regex, "-").toLowerCase();
 }
   
 var result = spinalCase('This Is Spinal Tap');
+var result2 = spinalCase('thisIsSpinalTap');
+var result3 = spinalCase('The_Andy_Griffith_Show');
+var result4 = spinalCase('Teletubbies say Eh-oh');
+var result5 = spinalCase('AllThe-small Things');
 
-console.log(result);
+console.log(result + "\n" + result2 + "\n" + result3 + "\n" + result4 + "\n" + result5);
