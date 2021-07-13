695deedf82941402f89176c86b863e7f96a7b72f
spacescript <simonbengoehring@gmail.com>
2021-07-13 16:09:43 -0700

    Update searchAndReplace.js

--- searchAndReplace.js
+++ searchAndReplace.js
@@ -1,7 +1,13 @@
 function myReplace(str, before, after) {
-    return str;
+    // var sentence = str.split(" ");
+    var index = str.indexOf(before);
+    // if (before[index] === before[index].toUpperCase()) {
+    //     return "Capitalized!"
+    // }
+    // return index;
+    return str[index] == str[index].toUpperCase();
 }
   
-var test = myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+var test = myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped");
 
 console.log(test);
