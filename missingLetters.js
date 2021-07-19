ec27332515fe62e7232b02be192b1bc514a03a8f
spacescript <simonbengoehring@gmail.com>
2021-07-19 10:49:20 -0700

    Update missingLetters.js

--- missingLetters.js
+++ missingLetters.js
@@ -1,11 +1,23 @@
 function fearNotLetter(str) {
-    //indexOf the string
-    //map charCodeAt 's
+    let missing = "Missing";
+    let map = Array.prototype.map
+    let numeralized = map.call(str, function(x) {
+        return x.charCodeAt(0)
+    })
+    for(let i = 1; i < numeralized.length - 1; i++) {
+        if(numeralized[i] != (numeralized[i-1] + 1)) {
+            return numeralized[i];
+        }
+    }
+    return missing;
+
     //if loop to check for incremental increases
     //draw fromCharCode of any missing increment
 
     // return str.charCodeAt(3);
     // return String.fromCharCode(100);
+    
+    // return a;
 }
   
 var test = fearNotLetter("abce");
