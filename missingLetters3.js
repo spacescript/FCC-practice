e90907f53d5755c217377273a77ca6b7fac83c98
spacescript <simonbengoehring@gmail.com>
2021-07-19 11:28:55 -0700

    Update missingLetters3.js

--- missingLetters3.js
+++ missingLetters3.js
@@ -1,5 +1,19 @@
 function fearNotLetter(str) {
-    return str;
+    var currCharCode = str.charCodeAt(0);
+    var missing = undefined;
+
+    str
+        .split("")
+        .forEach(letter => {
+            if(letter.charCodeAt(0) === currCharCode) {
+                currCharCode++;
+            } else {
+                missing = String.fromCharCode(currCharCode);
+            }
+        });
+    
+    return missing;
 }
   
-fearNotLetter("abce");
+var test = fearNotLetter("abce");
+console.log(test);
