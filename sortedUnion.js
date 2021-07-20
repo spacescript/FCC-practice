cc704fb93924136299022c8c992fe3103e11204c
spacescript <simonbengoehring@gmail.com>
2021-07-19 20:59:03 -0700

    Update sortedUnion.js

--- sortedUnion.js
+++ sortedUnion.js
@@ -1,5 +1,21 @@
 function uniteUnique(arr) {
-    return arr;
+    var set = [...arguments];
+    // var final = [];
+    var combo = [];
+    for(let i = 0; i < set.length; i++) {
+        combo.push(...(set[i]));
+    }
+    // Successfully got a single array of all values
+    // Now, I must use a map() or reduce() to remove all redundant values
+
+    var finish = combo.reduce(function(accu, curVal) {
+        if (accu.indexOf(curVal) === -1) {
+            accu.push(curVal)
+        }
+        return accu
+    }, [])
+    
+    return finish;
 }
   
 var test = uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
