fad06f1251950b035f381eb019c0769b1e369641
spacescript <simonbengoehring@gmail.com>
2021-07-20 18:32:07 -0700

    Update convertEntities.js

--- convertEntities.js
+++ convertEntities.js
@@ -1,5 +1,28 @@
 function convertHTML(str) {
-    return str;
+    var solution = str.split('');
+    for(let i = 0; i < solution.length; i++) {
+        switch(solution[i]) {
+            case "&":
+                solution[i] = "&amp;";
+                break;
+            case "<":
+                solution[i] = "&lt;";
+                break;
+            case ">":
+                solution[i] = "&gt;";
+                break;
+            case '"':
+                solution[i] = "&quot;";
+                break;
+            case "'":
+                solution[i] = "&apos;";
+                break;
+        }
+    }
+    solution = solution.join("");
+
+    return solution;
 }
   
-convertHTML("Dolce & Gabbana");
+var test = convertHTML("Dolce & Gabbana");
+console.log(test);
