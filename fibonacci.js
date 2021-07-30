b31dc7a082af7a03226e29eb7698ee1aebf14a2d
spacescript <simonbengoehring@gmail.com>
2021-07-29 20:09:18 -0700

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,12 @@
 function sumFibs(num) {
     var fibs = [1, 1];
-    for(let i = 1; i < num; i++) {
+
+    for(let i = 1; fibs[i] < num; i++) {
         fibs.push(i + fibs[(i-1)]);
     };
+
     return fibs
 }
   
-var test = sumFibs(12);
+var test = sumFibs(14);
 console.log(test);
