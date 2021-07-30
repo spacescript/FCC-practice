931874c40597e38dcfee5a60129e824e538b9a46
spacescript <simonbengoehring@gmail.com>
2021-07-30 12:35:29 -0700

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -2,10 +2,12 @@ function sumFibs(num) {
     var fibs = [1, 1];
 
     for(let i = 1; fibs[i] < num; i++) {
-        fibs.push(i + fibs[(i-1)]);
+        fibs.push(fibs[i] + fibs[(i-1)]);
     };
 
-    return fibs
+    fibs = fibs.filter(digi => digi <= num);
+    var answer = fibs.reduce((accu, curVal) => accu + curVal);
+    return answer;
 }
   
 var test = sumFibs(14);
