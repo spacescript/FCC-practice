788820d2600a2cb525802ec0ed1f06dbabe9705e
spacescript <simonbengoehring@gmail.com>
2021-07-30 22:41:59 -0700

    Update sumPrimes.js

--- sumPrimes.js
+++ sumPrimes.js
@@ -1,11 +1,11 @@
 function sumPrimes(num) {
-    var primeArray = [];
+    var primeArray = [1, 2];
 
     function isPrime(n) {
         // if (n < 2) return false;
-        var q = Math.floor(Math.sqrt(n));
-        for(let i = 2; i <= q; i++) {
-            if (n % i == 0) {
+        // var q = Math.floor(Math.sqrt(n));
+        for(let i = 2; i <= num; i++) {
+            if (n % i === 0) {
                 return false;
             }
             primeArray.push(n)
