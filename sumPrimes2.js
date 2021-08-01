bfea24a6e240d2ff1d444eb3e9981fdeae80b7cb
spacescript <simonbengoehring@gmail.com>
2021-08-01 13:08:01 -0700

    Update sumPrimes2.js

--- sumPrimes2.js
+++ sumPrimes2.js
@@ -1,5 +1,10 @@
 function sumPrimes(num) {
-    return sum;
+    let primes = [];
+    for (let i = 2; i <= num; i++) {
+        if(primes.every((prime) => i % prime !== 0))
+            primes.push(i);
+    }
+    return primes.reduce((sum, prime) => sum + prime, 0);
 }
   
 var test = sumPrimes(10);
