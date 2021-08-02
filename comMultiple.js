b553701503fa6bd297a9ca10383c0a7900c975b5
spacescript <simonbengoehring@gmail.com>
2021-08-01 20:47:49 -0700

    Update comMultiple.js

--- comMultiple.js
+++ comMultiple.js
@@ -1,5 +1,26 @@
 function smallestCommons(arr) {
-    return arr;
+    var ordered = arr.sort(function(a, b) {return a - b;});
+    var smallBoi = ordered[0];
+    var bigBoi = ordered[1];
+
+    var fullList = [];
+    for(let i = smallBoi; i <= bigBoi; i++) {
+        fullList.push(i);
+    }
+
+    // var bBMultiples = [];
+    // for(let i = 1; i <= (bigBoi * bigBoi); i++) {
+    //     bBMultiples.push((bigBoi * i));
+    // }
+
+    let upperBound = 1;
+    for (let i = smallBoi; i <= bigBoi; i++) {
+        upperBound *= i;
+    }
+
+    
+
+    return upperBound;
 }
   
   
