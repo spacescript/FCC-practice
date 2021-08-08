6d4abbede0b96a622833d375faf84593c5dd0b41
spacescript <simonbengoehring@gmail.com>
2021-08-07 19:42:31 -0700

    Update dropIt.js

--- dropIt.js
+++ dropIt.js
@@ -1,7 +1,15 @@
 function dropElements(arr, func) {
-    return arr;
+    var set = arr.map(x => func(x));
+    var answers = [];
+    for(let i = 0; i < set.length; i++) {
+        if(set[i] == true) {
+            answers.push(arr[i])
+        }
+    }
+    return answers;
 }
   
 var test = dropElements([1, 2, 3], function(n) {return n < 3; });
+// var test = dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;});
 
 console.log(test);
