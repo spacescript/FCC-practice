e635e40a27f5ca132c309f392986d7857b6c488d
spacescript <simonbengoehring@gmail.com>
2021-08-22 16:06:26 -0700

    Update everythingTrue.js

--- everythingTrue.js
+++ everythingTrue.js
@@ -1,11 +1,13 @@
 function truthCheck(collection, pre) {
     var counter = 0;
 
-    for( in ) {
-        
+    for(let v in collection) {
+        if (collection[v].hasOwnProperty(pre) && Boolean(collection[v][pre])) {
+            counter++;
+        }
     }
 
-    return pre;
+    return counter == collection.length;
 }
   
 var test = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
