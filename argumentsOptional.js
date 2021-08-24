4fdec430e2ecfcce0704d8f3092c40920199944c
spacescript <simonbengoehring@gmail.com>
2021-08-23 23:38:40 -0700

    Update argumentsOptional.js

--- argumentsOptional.js
+++ argumentsOptional.js
@@ -1,5 +1,10 @@
 function addTogether() {
-    if(arguments == );
+    if(arguments.length == 2) {
+        if(isNaN(arguments[0]) || isNaN(arguments[1])) {
+            return "Check!"
+        }
+    }
+    return "Fail";
 }
   
 var test = addTogether(2,3);
