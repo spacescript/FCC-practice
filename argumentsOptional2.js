b613ddc92dcc58290d6b224781308bbcf9fc8dd9
spacescript <simonbengoehring@gmail.com>
2021-08-24 20:31:01 -0700

    Update argumentsOptional2.js

--- argumentsOptional2.js
+++ argumentsOptional2.js
@@ -1,5 +1,12 @@
 function addTogether() {
-    return false;
+    const [first, second] = arguments;
+  if (typeof(first) !== "number")
+    return undefined;
+  if (second === undefined)
+    return (second) => addTogether(first, second);
+  if (typeof(second) !== "number")
+    return undefined;
+  return first + second;
 }
   
 addTogether(2,3);
