44dea6d9d37b077d97a9f52f4c0bd472ec146227
spacescript <simonbengoehring@gmail.com>
2021-09-13 14:56:03 -0700

    Update makeperson.js

--- makeperson.js
+++ makeperson.js
@@ -2,7 +2,7 @@ var Person = function(firstAndLast) {
     // Only change code below this line
     // Complete the method below and implement the others similarly
     var words = firstAndLast.split(" ");
-    // var [firstName, lastName] = [words[0], words[1]];
+    var [baseFirstName, baseLastName] = [words[0], words[1]];
 
     this.getFirstName = function() {
       var [firstName, lastName] = [words[0], words[1]];
@@ -21,11 +21,23 @@ var Person = function(firstAndLast) {
 
     this.setFirstName = function(first) {
       var [firstName, lastName] = [first, words[1]];
+      return first;
     }
 
-    return firstAndLast;
+    this.setLastName = function(last) {
+      var [firstName, lastName] = [words[0], last];
+      return last;
+    }
+
+    this.setFullName = function(firstAndLast2) {
+      var names = firstAndLast2.split(" ");
+      var [firstName, lastName] = [names[0], names[1]];
+      return firstName + lastName;
+    }
+
+    return baseFirstName + baseLastName;
 };
   
 var bob = new Person('Bob Ross');
-var test = bob.setFirstName('Jack');
+var test = bob.setLastName('Jack London');
 console.log(test);
