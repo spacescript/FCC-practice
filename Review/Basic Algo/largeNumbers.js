1aaee4c0ddfe9677c9e4f98a6611bf2dfcaf7a63
spacescript <simonbengoehring@gmail.com>
2021-11-09 15:49:17 -0800

    Update largeNumbers.js

--- Review/Basic Algo/largeNumbers.js
+++ Review/Basic Algo/largeNumbers.js
@@ -1,13 +1,15 @@
 function largestOfFour(arr) {
     let answer = [];
-    // for(let i = 0; i < arr.length; i++){
-    //     answer.push(Math.max(arr[i]));
-    // }
-    // return answer;
+    for(let i = 0; i < arr.length; i++){
+        answer.push(Math.max(...arr[i].flat()));
+    }
+    return answer;
     // return Math.max(arr[0].flat());
+    // const firstArr = (arr[0].flat().map(Number));
+    // return Math.max(...arr[0].flat());
     // return arr[0].flat();
     // return typeof(arr[0].flat()[0]);
-    return arr[0].flat()[0];
+    // return arr[0].flat()[1];
 }
   
 const test = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
