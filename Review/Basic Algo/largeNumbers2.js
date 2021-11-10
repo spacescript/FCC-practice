144396e2f728c629c50069142feeb8958ec6b855
spacescript <simonbengoehring@gmail.com>
2021-11-09 16:52:45 -0800

    Update largeNumbers2.js

--- Review/Basic Algo/largeNumbers2.js
+++ Review/Basic Algo/largeNumbers2.js
@@ -1,7 +1,12 @@
 function largestOfFour(arr) {
     let answer = [];
-    for(let i = 0; )
-    return arr;
+    for(let i = 0; i < arr.length; i++) {
+        let largestNumber = 0;
+        for(let j = 0; j < arr[i].length; j++) {
+            
+        }
+    }
+    return answer;
 }
   
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
