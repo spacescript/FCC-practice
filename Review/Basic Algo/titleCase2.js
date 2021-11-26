5df6bfefd8f389b1c82457500550c3a771fb1580
spacescript <simonbengoehring@gmail.com>
2021-11-26 13:41:11 -0800

    Nov Prac Package 4

--- Review/Basic Algo/titleCase2.js
+++ Review/Basic Algo/titleCase2.js
@@ -1,6 +1,11 @@
 function titleCase(str) {
-    
-    return str.charAt(0).toUpperCase() + str.slice(1);
+    const mySet = str.split(' ');
+    let newSentence = [];
+    for(let i = 0; i < mySet.length; i++) {
+        newSentence.push(mySet[i].charAt(0).toUpperCase() + str.slice(1));
+        // return newSentence.push(mySet[i]);
+    }
+    return newSentence;
 }
   
 const test = titleCase("I'm a little tea pot");
