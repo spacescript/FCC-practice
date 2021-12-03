2820108dcfb973673e5f7f41d301534591f42ded
spacescript <simonbengoehring@gmail.com>
2021-12-03 15:12:01 -0800

    Create mutations2.js

---
+++ Review/Basic Algo/mutations2.js
@@ -0,0 +1,11 @@
+function mutation(arr) {
+    return arr[1]
+        .toLowerCase()
+        .split('')
+        .every(function(letter) {
+            return arr[0].toLowerCase().indexOf(letter) != -1;
+        })
+}
+  
+const test = mutation(["hello", "hey"]);
+console.log(test);
