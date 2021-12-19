ca5ea712eda5451ad4b5b273a1e79851c818a3a7
spacescript <simonbengoehring@gmail.com>
2021-12-18 18:33:59 -0800

    Update inventMap.js

--- Review/Func Prog Review/inventMap.js
+++ Review/Func Prog Review/inventMap.js
@@ -4,8 +4,8 @@ const s = [23, 65, 98, 5];
 Array.prototype.myMap = function(callback) {
   const newArray = [];
   // Only change code below this line
-    for(let i = 0; i < callback.length; i++) {
-        newArray.push(callback[i])
+    for(let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i]));
     }
   // Only change code above this line
   return newArray;
@@ -14,3 +14,5 @@ Array.prototype.myMap = function(callback) {
 const new_s = s.myMap(function(item) {
   return item * 2;
 });
+
+console.log(new_s);
