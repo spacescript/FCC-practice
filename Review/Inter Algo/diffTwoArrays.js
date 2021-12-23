93a2f8f398940b71f41d2b8a81c334eedd93c477
spacescript <simonbengoehring@gmail.com>
2021-12-22 23:51:08 -0800

    Dec Prac Package 3

---
+++ Review/Inter Algo/diffTwoArrays.js
@@ -0,0 +1,12 @@
+function diffArray(arr1, arr2) {
+    for(let i = 0; i < arr1.length; i++) {
+        for(let j = 0; j < arr2.length; i++) {
+            if(indexOf(arr1[i]) )
+        }
+    }
+    const newArr = [];
+    return newArr;
+}
+  
+const test = diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(test);
---
+++ Review/Inter Algo/diffTwoArrays2.js
@@ -0,0 +1,7 @@
+function diffArray(arr1, arr2) {
+    
+    const newArr = [];
+    return newArr;
+}
+  
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
