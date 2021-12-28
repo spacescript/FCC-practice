a093b3902fef4958e9fceccc8807ba9ab407f8e4
spacescript <simonbengoehring@gmail.com>
2021-12-27 18:27:20 -0800

    Update diffTwoArrays.js

--- Review/Inter Algo/diffTwoArrays.js
+++ Review/Inter Algo/diffTwoArrays.js
@@ -1,17 +1,24 @@
 function diffArray(arr1, arr2) {
     const newArr = [];
-    for(let i = 0; i < arr1.length; i++) {
-        for(let j = 0; j < arr2.length; i++) {
-            if(arr1[i] !== arr2[j] && j == (arr2.length - 1)) {
-                newArr.push(arr1[i]);
-            }
-            if(arr2[j] !== arr1[i] && i == (arr1.length - 1)) {
-                newArr.push(arr2[j]);
-            }
-        }
+    // for(let i = 0; i < arr1.length; i++) {
+    //     for(let j = 0; j < arr2.length; i++) {
+    //         if(arr1[i] !== arr2[j] && j == (arr2.length - 1)) {
+    //             newArr.push(arr1[i]);
+    //         }
+    //         if(arr2[j] !== arr1[i] && i == (arr1.length - 1)) {
+    //             newArr.push(arr2[j]);
+    //         }
+    //     }
+    // }
+    let tempArr = [...arr1, ...arr2];
+    for(let i = 0; i < tempArr.length; i++) {
+        let count = 0;
+        if (tempArr[i] === )
     }
+
+
     // if((indexOf(arr1[i])) !== (indexOf(arr2[j])))
-    return newArr;
+    return tempArr;
 }
   
 const test = diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
