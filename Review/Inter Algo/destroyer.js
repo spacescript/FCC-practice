fa2ec6e4403dff612762523740efd7d9ba210b22
spacescript <simonbengoehring@gmail.com>
2021-12-29 18:36:31 -0800

    Update destroyer.js

--- Review/Inter Algo/destroyer.js
+++ Review/Inter Algo/destroyer.js
@@ -1,11 +1,8 @@
 function destroyer(arr) {
-    // var list = [...arguments[0]];
-
-    for(let i = 1; i < arguments.length; i++) {
-        arr.map(a => a.splice(arr.indexOf(arguments[i]), 1));
-    }
-
-    return arr;
+    var toRemove = Array.from(arguments).slice(1);
+    return arr.filter(function(val) {
+        return !toRemove.includes(val);
+    });
 }
   
 const test = destroyer([1, 2, 3, 1, 2, 3], 2, 3);
