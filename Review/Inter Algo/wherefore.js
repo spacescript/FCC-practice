c146267f3b852ad3135792bc62dca76a7755c4e7
spacescript <simonbengoehring@gmail.com>
2022-01-03 19:59:46 -0800

    Update wherefore.js

--- Review/Inter Algo/wherefore.js
+++ Review/Inter Algo/wherefore.js
@@ -1,13 +1,22 @@
 function whatIsInAName(collection, source) {
-    const arr = [];
+    // const arr = [];
     // Only change code below this line
-    let myKey = Object.keys(source);
+    let sourceKey = Object.keys(source);
     // for(let i = 0; i < Object.keys(collection).length; i++) {
     //     if(collection[i].hasOwnProperty(myKey) && collection[i].myKey == source) {
     //         arr.push(collection[i]);
     //     }
     // }
-    return collection[0].myKey;
+
+    return collection.filter(function(obj) {
+        for(var i = 0; i < sourceKey.length; i++) {
+            if(
+                !obj.hasOwnProperty(sourceKey[i]) ||
+                obj[sourceKey[i]] !== source[sourceKey[i]]
+            ) {return false;}
+        }
+        return true;
+    });
 
     // Only change code above this line
     // return arr;
