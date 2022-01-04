3e7817644061767697f5c2a94633b05f3ebb8c19
spacescript <simonbengoehring@gmail.com>
2022-01-04 14:27:31 -0800

    Create pigLatin2.js

---
+++ Review/Inter Algo/pigLatin2.js
@@ -0,0 +1,13 @@
+function translatePigLatin(str) {
+    let consonantRegex = /^[^aeiou]+/;
+    let myConsonants = str.match(consonantRegex);
+    return myConsonants !== null
+        ? str
+            .replace(consonantRegex, "")
+            .concat(myConsonants)
+            .concat("ay")
+        : str.concat("way");
+}
+  
+const test = translatePigLatin("eight");
+console.log(test);
