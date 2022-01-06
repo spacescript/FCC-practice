6b49e9e0f93fef7bdf6983488b5cb71354c416e7
spacescript <simonbengoehring@gmail.com>
2022-01-05 16:22:05 -0800

    Update searchReplace.js

--- Review/Inter Algo/searchReplace.js
+++ Review/Inter Algo/searchReplace.js
@@ -3,14 +3,14 @@ function myReplace(str, before, after) {
     var findBefore = splitStr.indexOf(before);
     str.replace(before, after);
     var newAfter = after.split("");
+    // var
     // var newBefore = before.split("");
-    // if(before.charCodeAt(0) >= 65 && before.charCodeAt(0) <= 90) {
-    //     newAfter = after[0].toUpperCase();
-    // } else {
-    //     newBefore = after[0].toLowerCase();
-    // }
-    // return str.replace(newBefore, newAfter);
-    return newAfter;
+    if(before.charCodeAt(0) >= 65 && before.charCodeAt(0) <= 90) {
+        newAfter[0].toUpperCase();
+    } else {
+        newAfter[0].toLowerCase();
+    }
+    return str.replace(before, newAfter);
 }
   
 const test = myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
