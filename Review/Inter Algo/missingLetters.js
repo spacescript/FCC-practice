bacc3f12f07b0e6274c65b7105cedc7597887ae8
spacescript <simonbengoehring@gmail.com>
2022-01-08 16:41:03 -0800

    Update missingLetters.js

--- Review/Inter Algo/missingLetters.js
+++ Review/Inter Algo/missingLetters.js
@@ -1,16 +1,17 @@
 function fearNotLetter(str) {
-    // var mySet = str.split("");
-    var answer = [];
-    var testArr = [97, 98, 99, 100]
-    for(let i = 0; i < str.length; i++) {
-        answer.push(str.charCodeAt(i));
-    }
-    return testArr.every(function(x,y) {
-        if (y === x+1) {
-            return true;
-        }
-        return false;
-    });
+    let currCharCode = str.charCodeAt(0);
+    let missing = undefined;
+    
+    str
+        .split("")
+        .forEach(letter => {
+            if(letter.charCodeAt(0) === currCharCode) {
+                currCharCode++;
+            } else {
+                missing = String.fromCharCode(currCharCode);
+            }
+        });
+    return missing;
 }
   
 const test = fearNotLetter("abce");
