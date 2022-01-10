3141add68ff9553a634e1029c4cc122e1ee2f6c7
spacescript <simonbengoehring@gmail.com>
2022-01-10 13:04:24 -0800

    Update missingLetters3.js

--- Review/Inter Algo/missingLetters3.js
+++ Review/Inter Algo/missingLetters3.js
@@ -1,5 +1,9 @@
 function fearNotLetter(str) {
-    return str;
+    for(let i = 0; i< str.length; i++) {
+        if(str.charCodeAt(i) - str.charCodeAt(i - 1) > 1) {
+            return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+        }
+    }
 }
   
 const test = fearNotLetter("abce");
