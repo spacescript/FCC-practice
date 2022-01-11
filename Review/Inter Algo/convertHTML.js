7d5775d7c5dc7dc31f4904aba7d496df09a77f3c
spacescript <simonbengoehring@gmail.com>
2022-01-11 12:20:34 -0800

    Update convertHTML.js

--- Review/Inter Algo/convertHTML.js
+++ Review/Inter Algo/convertHTML.js
@@ -1,5 +1,25 @@
 function convertHTML(str) {
-    return str;
+    var mySet = str.split('');
+    for(let i = 0; i < mySet.length; i++) {
+        switch(mySet[i]) {
+            case '&':
+                mySet[i] = '&amp;';
+                break;
+            case '<':
+                mySet[i] = '&lt;';
+                break;
+            case '>':
+                mySet[i] = '&gt;';
+                break;
+            case '"':
+                mySet[i] = '&quot;';
+                break;
+            case "'":
+                mySet[i] = '&apos;';
+                break;
+        }
+    }
+    return mySet.join('');
 }
   
 const test = convertHTML("Dolce & Gabbana");
