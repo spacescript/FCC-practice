dc5d86878815d1e9a9a0e7a0ed1961a212a06d6b
spacescript <simonbengoehring@gmail.com>
2022-01-11 16:18:39 -0800

    Create fibonacci3.js

---
+++ Review/Inter Algo/fibonacci3.js
@@ -0,0 +1,18 @@
+function sumFibs(num) {
+    var prevNum = 0;
+    var currNum = 1;
+    var result = 0;
+
+    while(currNum <= num) {
+        if(currNum % 2 !== 0) {
+            result += currNum;
+        }
+        currNum += prevNum;
+        prevNum = currNum - prevNum;
+    }
+
+    return result;
+}
+  
+const test = sumFibs(10);
+console.log(test);
