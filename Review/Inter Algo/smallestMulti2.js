961c87741acd33b95e159fc352adecae45fe4859
spacescript <simonbengoehring@gmail.com>
2022-01-13 16:51:16 -0800

    Update smallestMulti2.js

--- Review/Inter Algo/smallestMulti2.js
+++ Review/Inter Algo/smallestMulti2.js
@@ -1,6 +1,23 @@
 function smallestCommons(arr) {
-    
-    return arr;
+    const [min, max] = arr.sort((a,b) => a - b);
+    const numberDivisors = max - min + 1;
+
+    let upperBound = 1;
+    for(let i = min; i <= max; i++) {
+        upperBound *= i;
+    }
+
+    for (let multiple = max; multiple <= upperBound; multiple += max) {
+        let divisorCount = 0;
+        for(let i = min; i <= max; i++) {
+            if(multiple % i === 0) {
+                divisorCount += 1;
+            }
+        }
+        if (divisorCount === numberDivisors) {
+            return multiple;
+        }
+    }
 }
   
 const test = smallestCommons([1,5]);
