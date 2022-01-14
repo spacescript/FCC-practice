4997747c1417694f14a96a50d48d880953279fd2
spacescript <simonbengoehring@gmail.com>
2022-01-14 13:48:12 -0800

    Update steamroller.js

--- Review/Inter Algo/steamroller.js
+++ Review/Inter Algo/steamroller.js
@@ -1,13 +1,17 @@
 function steamrollArray(arr) {
     // return Array.isArray(arr[1]);
-    // var newArr = [];
-    // if(Array.isArray(arr[0])) {
-    //     newArr.push(arr[0]);
-    //     arr.slice
-    //     steamrollArray(arr);
-    // }
-    arr.splice(1);
-    return arr;
+    var newArr = [];
+    if(Array.isArray(arr[0])) {
+        newArr.push(arr[0]);
+        arr.splice(0,1);
+        steamrollArray(arr);
+    }
+
+    return newArr;
+    // arr.splice(0,1);
+    // return arr;
+    // newArr.push(arr[0]);
+    // return arr;
 }
 
 const test = steamrollArray([1, [2], [3, [[4]]]]);
