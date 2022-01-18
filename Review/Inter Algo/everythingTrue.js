0372b625d093f1ce970726586911481632e0171d
spacescript <simonbengoehring@gmail.com>
2022-01-17 18:09:40 -0800

    Update everythingTrue.js

--- Review/Inter Algo/everythingTrue.js
+++ Review/Inter Algo/everythingTrue.js
@@ -1,11 +1,15 @@
 function truthCheck(collection, pre) {
-    var answer = [];
-    for(let i = 0; i < collection.length; i++) {
-        // if(collection[i].hasOwnProperty(pre)) {numberAnswers += 1};
-        answer.push(Object.entries(collection[i]));
-    }
-    //  return collection.every(item => item.hasOwnProperty(pre));
-    return answer;
+    // var answer = [collection[1][pre]];
+    // for(let i = 0; i < collection.length; i++) {
+    //     // if(collection[i].hasOwnProperty(pre)) {numberAnswers += 1};
+    //     answer.push(collection.map(x => x.pre));
+    // }
+    // if(collection.every(item => item.hasOwnProperty(pre))) {
+    //     for(let i = 0; i < collection.length; i++) {
+    //         return collection.every()
+    //     }
+    // };
+    return collection.every(item => item.hasOwnProperty(pre)) && collection.every(item => item[pre] == true);
 }
   
 const test = truthCheck([{"single": ""}, {"single": "double"}], "single");
