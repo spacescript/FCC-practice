4899ed9f99b16221960d9fd319f2f879327cf601
spacescript <simonbengoehring@gmail.com>
2022-01-19 12:52:38 -0800

    Update optional.js

--- Review/Inter Algo/optional.js
+++ Review/Inter Algo/optional.js
@@ -1,8 +1,42 @@
 function addTogether() {
-    // return arguments.length;
-    var nums = arguments.map(x => nums.push(x));
-    return nums;
+    // My plan:
+    // 1) Identify arguments
+        // A) Confirm # of args
+        // B) Confirm if args are numbers
+    // 2) if args.length < 2 then -->
+        // A) return a new function
+    // 3) if args.length = 2 then -->
+        // A) Add together and return
+
+    if (arguments.length == 1) {
+        var first = arguments[0];
+        // if(typeof(first) != "number") {
+        //     return undefined;
+        // } else {
+        //     var partial = function continueSum(num) {
+        //         // return num + first;
+        //         return "Here!";
+        //     }
+        // }
+        if(typeof(first) == "number") {
+            var partial = function continueSum(num) {
+                // return num + first;
+                return "here";
+            }
+        } else {
+            return undefined;
+        }
+        return partial;
+    } else if (arguments.length == 2) {
+        var first = arguments[0], second = arguments[1];
+        if (typeof(first) == "number" && typeof(second) == "number") {
+            return first + second;
+            // return "Here!";
+        } else {
+            return undefined;
+        }
+    }
 }
   
-const test = addTogether(2,3);
+const test = addTogether(2)([3]);
 console.log(test);
