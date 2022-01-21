1eb29a6ba49f69fecc6ce174f410fa619ce96440
spacescript <simonbengoehring@gmail.com>
2022-01-21 15:56:52 -0800

    Create romanNumerals.js

---
+++ Review/Inter Algo/romanNumerals.js
@@ -0,0 +1,13 @@
+function convertToRoman(num) {
+    // First Plan:
+    // 1) Make counters for each denomination to be counted
+    // 2) Recursive solution?
+    // 3) Use an if function to determine if an M can be pulled out of the number
+        // A) Subtract out the M, and increase counter
+        // B) Check to see if the next lowest denomination can be used for a "less than" numeral
+    // --> Recurse, repeat M's, move down to C's
+
+    return num;
+}
+   
+convertToRoman(36);
