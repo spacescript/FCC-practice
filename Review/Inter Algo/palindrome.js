7e65408668f30191553c090b1d3c8ba49b2f8d6d
spacescript <simonbengoehring@gmail.com>
2022-01-21 12:10:23 -0800

    Update palindrome.js

--- Review/Inter Algo/palindrome.js
+++ Review/Inter Algo/palindrome.js
@@ -1,7 +1,17 @@
 function palindrome(str) {
     var regex = /\W/g;
     var mySet = str.replace(regex, '').toLowerCase();
-    return mySet;
+    var middle = Math.floor(mySet.length / 2);
+
+    for(let i = 0; i < mySet.length; i++) {
+        for(let j = mySet.length - 1; j > -1; j--) {
+            if(mySet.charCodeAt(i) != mySet.charCodeAt(j)) {return false}
+            // return "Charcode i = " + mySet.charCodeAt(i) + " Charcode j = " + mySet.charCodeAt(j);
+        }
+    }
+    return true;
+
+    return middle;
 }
   
 const test = palindrome("A man, a plan, a canal. Panama");
