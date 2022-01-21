eec7705ae35f0540ab7121efeddb2fa01226f8a6
spacescript <simonbengoehring@gmail.com>
2022-01-21 15:29:32 -0800

    Update palindrome.js

--- Review/Inter Algo/palindrome.js
+++ Review/Inter Algo/palindrome.js
@@ -1,22 +1,38 @@
 function palindrome(str) {
-    var regex = /\W/g;
+    var regex = /[\W_]+/g;
     var mySet = str.replace(regex, '').toLowerCase();
     var middle = Math.floor(mySet.length / 2);
-    var searcher = [];
+    var start = 0, end = mySet.length - 1, searcher = [];
 
-    // for(let i = 0; i < middle; i++) {
-    //     for(let j = mySet.length - 1; j >= middle; j--) {
-    //         if(mySet.charCodeAt(i) != mySet.charCodeAt(j)) {searcher.push("False at: " + i + " and " + j)}
-    //         searcher.push(true);
-    //         // return "Charcode i = " + mySet.charCodeAt(i) + " Charcode j = " + mySet.charCodeAt(j);
-    //     }
-    // }
+    for(let i = 0; i <= middle; i++) {
+        // if((mySet.charCodeAt(start + i)) == (mySet.charCodeAt(end - i))) {
+        //     return true;
+        // }
+        // return false;
+        searcher.push([mySet.charCodeAt(start + i), mySet.charCodeAt(end - i)]);
+    }
+
+    var key = searcher.map(function(array) {
+        if(array[0] == array[1]) {
+            return true;
+        }
+        return false;
+    });
+
+    return key.every(x => x ==true);
+
+    // if([mySet.charCodeAt(start + 4) == mySet.charCodeAt(end - 4)]) {return true};
+
+    // var fourTestStart = mySet.charCodeAt(start + 4);
+    // var fourTestEnd = mySet.charCodeAt(end - 4);
+
+    // return "Start: " + fourTestStart + " End: " + fourTestEnd;
+    // if(fourTestStart == fourTestEnd) {return true} else {return false};
+
     // return searcher;
-
-    // return middle;
-
-    return mySet.length;
+    // return mySet.charCodeAt(start) + " and " + mySet.charCodeAt(end);
+    // return fourTestStart + " and " + fourTestEnd;
 }
   
-const test = palindrome("A man, a plan, a canal. Panama");
+const test = palindrome("almostomla");
 console.log(test);
