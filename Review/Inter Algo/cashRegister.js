4d56125ed9a62cac44034b87818765b0d288191c
spacescript <simonbengoehring@gmail.com>
2022-01-26 19:30:30 -0800

    Update cashRegister.js

--- Review/Inter Algo/cashRegister.js
+++ Review/Inter Algo/cashRegister.js
@@ -3,9 +3,9 @@ function checkCashRegister(price, cash, cid) {
 
     var initialCounters = [cid[0][1] / .01, Math.round( cid[1][1] / .05 ), cid[2][1] / .1, cid[3][1] / .25, cid[4][1] / 1, cid[5][1] / 5, cid[6][1] / 10, cid[7][1] / 20, cid[8][1] / 100];
 
-    // var counters = [cid[0][1] / .01, Math.round( cid[1][1] / .05 ), cid[2][1] / .1, cid[3][1] / .25, cid[4][1] / 1, cid[5][1] / 5, cid[6][1] / 10, cid[7][1] / 20, cid[8][1] / 100];
+    var counters = [cid[0][1] / .01, Math.round( cid[1][1] / .05 ), cid[2][1] / .1, cid[3][1] / .25, cid[4][1] / 1, cid[5][1] / 5, cid[6][1] / 10, cid[7][1] / 20, cid[8][1] / 100];
 
-    var counters = initialCounters;
+    // var counters = initialCounters;
     var changeOwed = cash - price;
     
 
@@ -53,7 +53,24 @@ function checkCashRegister(price, cash, cid) {
 
     if(counters.every(x => x >= 0) == false) {
         return {status: "INSUFFICIENT_FUNDS", change: []}
-    } else if (counters.every(x => x === 0)) {
+    } 
+    // else if (counters.every(x => x === 0)) {
+        
+    // }
+
+    // var difference = counters.map(function(x){
+    //     return counters[x] === initialCounters[x];
+    // });
+
+    let difference = [];
+
+    for(let i = 0; i < counters.length; i++) {
+        if(counters[i] != initialCounters[i]) {
+            difference.push([i, counters[i]]);
+        }
+    }
+
+    for(let i = 0; i < difference.length; i++) {
         
     }
 
@@ -61,10 +78,12 @@ function checkCashRegister(price, cash, cid) {
 
     // Notes: Use every() on counters to check if every counter = 0 for the "CLOSED" result. Otherwise, check to see if any of the values are negative. If any of them are, return the "INSUFFICIENT" result. Otherwise, return myCid with altered values.
 
-    return counters;
+    // return initialCounters;
+    // return counters;
+    return difference;
 }
   
-const test = checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+const test = checkCashRegister(18.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 console.log(test);
 
 // function payout(digit) {
